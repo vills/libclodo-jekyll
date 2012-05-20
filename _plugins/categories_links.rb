da964c14b293cf64b3d91154d38ae070c379ced8
Vil Surkin <vill.srk@gmail.com>
2012-05-20 22:42:48 +0400

    added liquid helper 'catname2link'

--- _plugins/categories_links.rb
+++ _plugins/categories_links.rb
@@ -2,47 +2,6 @@
 
 module Jekyll
 
-	# added accessor 'categories_links'
-	class Site
-		attr_accessor :config, :layouts, :posts, :pages, :static_files,
-		              :categories, :categories_links, :exclude, :include, :source, :dest, :lsi, :pygments,
-		              :permalink_style, :tags, :time, :future, :safe, :plugins, :limit_posts
-
-		def read_posts(dir)
-		  base = File.join(self.source, dir, '_posts')
-		  return unless File.exists?(base)
-		  entries = Dir.chdir(base) { filter_entries(Dir['**/*']) }
-
-		  self.categories_links = Array.new
-
-		  # first pass processes, but does not yet render post content
-		  entries.each do |f|
-		    if Post.valid?(f)
-		      post = Post.new(self, self.source, dir, f)
-
-		      if post.published && (self.future || post.date <= self.time)
-		        self.posts << post
-		        post.categories.each { |c|
-		        	self.categories_links << catname2link(c) # => added this string
-		        	self.categories[c] << post
-		        }
-		        post.tags.each { |c| self.tags[c] << post }
-		      end
-		    end
-		  end
-
-		  self.posts.sort!
-
-		  # limit the posts if :limit_posts option is set
-		  if limit_posts
-		    limit = self.posts.length < limit_posts ? self.posts.length : limit_posts
-		    self.posts = self.posts[-limit, limit]
-		  end
-		end
-
-	end
-
-
 	class Post
 		def catname2link(category)
 			require "#{self.site.source}/_catlinks"
@@ -84,6 +43,15 @@ module Jekyll
 		  @url = url.split('/').reject{ |part| part =~ /^\.+$/ }.join('/')
 		  @url += "/" if url =~ /\/$/
 		  @url
+		end
+
+	end
+
+
+	module Filters
+		
+		def catname2link(category)
+			return @context.registers[:site].catname2link(category)
 		end
 
 	end
