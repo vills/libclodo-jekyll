ae19bfbad0879a70eb2b8fc03c49c352901d855f
Vil Surkin <vill.srk@gmail.com>
2012-06-01 20:22:24 +0400

    1.9 & 1.8 ruby's versions fix 2 :-)

--- _plugins/categories_links.rb
+++ _plugins/categories_links.rb
@@ -6,7 +6,12 @@ module Jekyll
 		def catname2link(category)
 			require "#{self.site.source}/_catlinks"
 
-			t = category_links.invert.key(category)
+			if Hash.method_defined? :key
+	          t = category_links.invert.key(category)
+	        else
+	          t = category_links.invert.index(category)
+	        end
+        
 			return category if t.nil?
 			t
 		end
